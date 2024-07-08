5df4e6a4f84a49920785bdb1897ad749e7011df8
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2024-07-08 10:51:39 +0200

    Get Py 3.8 EOL from the web

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,22 @@
 """Sphinx config."""
 
+import requests
+
 copyright = "@webknjaz"
 html_theme = 'furo'
 project = 'Lokiverse'
+
+
+def _identify_py38_eol_date():
+    py38_lifecycle_details = requests.get(
+        'https://endoflife.date/api/python/3.8.json',
+        headers={'Accept': 'application/json'},
+        timeout=5,
+    )
+    return py38_lifecycle_details.json()['eol']
+
+
+def setup(app):
+    app.config.rst_epilog = f"""
+    .. |py38_eol_date| replace:: {_identify_py38_eol_date()}
+    """
--- docs/index.rst
+++ docs/index.rst
@@ -3,3 +3,5 @@ Lokiverse
 =========
 
 This is a sample project for DIY lock files.
+
+This project is going to be supported until |py38_eol_date|.
